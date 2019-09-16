64e56ed6480c8c9e82e2420f169d45bc94884bb4
Amin Shazrin <aminshazrin@yahoo.com>
2019-09-16 17:23:44 +0800

    Done Routes Testing

--- tests/test-route.test.js
+++ tests/test-route.test.js
@@ -12,6 +12,13 @@ describe("Custom Code Editor : Routes Saving Options", function () {
     // Ends everything at 50 seconds
     this.timeout(50000);
 
+    var dummyUser;
+    var body = {
+        "customCodeEditor": {
+            "enableEmmet": true
+        }
+    }
+
     after(function (done) {
         try {
             require("apostrophe/test-lib/util").destroy(apos, done);
@@ -53,26 +60,131 @@ describe("Custom Code Editor : Routes Saving Options", function () {
         });
     });
 
-  // Test pieces.newInstance()
-  it('should be able to insert a new user', function (done) {
-      assert(apos.users.newInstance);
-      var user = apos.users.newInstance();
-      assert(user);
+    // Test pieces.newInstance()
+    it('should be able to insert a new user', function (done) {
+        assert(apos.users.newInstance);
+        var user = apos.users.newInstance();
+        assert(user);
 
-      user.firstName = 'Jane';
-      user.lastName = 'Doe';
-      user.title = 'Jane Doe';
-      user.username = 'JaneD';
-      user.password = '123password';
-      user.email = 'jane@aol.com';
+        user.firstName = 'Abu';
+        user.lastName = 'Bakar';
+        user.title = 'Abu Bakar';
+        user.username = 'abuBakar';
+        user.password = '123password';
+        user.email = 'abu@dummy.com';
 
-      assert(user.type === 'apostrophe-user');
-      assert(apos.users.insert);
-      apos.users.insert(apos.tasks.getReq(), user, function (err) {
-          assert(!err);
-          janeOne = user;
-          done();
-      });
+        assert(user.type === 'apostrophe-user');
+        assert(apos.users.insert);
+        apos.users.insert(apos.tasks.getReq(), user, function (err) {
+            assert(!err);
+            dummyUser = user;
+            done();
+        });
+    });
 
-  });
+    it('should get empty user save options on customCodeEditor' , function(done){
+        var req = apos.tasks.getReq();
+        var existingUser = _.cloneDeep(req.user);
+        var myUser = _.cloneDeep(dummyUser);
+        req.user = _.assign(existingUser, myUser);
+        apos.customCodeEditor.getOptions(req, function(err, result){
+            if(err){
+                console.log("ERROR (GET) : ", err)
+            }
+            assert(!err)
+            expect(result).toMatchObject({});
+            expect(Object.keys(result).length).toBe(0);
+            done();
+        })
+    });
+
+    it('should save user options successfully', function(done) {
+        var req = apos.tasks.getReq();
+        var existingUser = _.cloneDeep(req.user);
+        var myUser = _.cloneDeep(dummyUser);
+        req.user = _.assign(existingUser, myUser);
+        req.body = _.cloneDeep(body)
+        apos.customCodeEditor.submit(req, function(err) {
+            if(err) {
+                console.log("ERROR (POST) : ", err)
+            }
+            assert(!err);
+            // Check User Database to be match with customCodeEditor saving options
+            apos.users.find(req , {username : "abuBakar"}).toObject(function(err, user) {
+                if(err) {
+                    console.log("ERROR (USER POST) : ", err)
+                }
+                assert(!err);
+                expect(user.customCodeEditor).toMatchObject(body.customCodeEditor);
+                assert(user.username === "abuBakar");
+                done()
+            })
+        })
+    });
+
+    it('should get previous saves options', function(done) {
+        var req = apos.tasks.getReq();
+        var existingUser = _.cloneDeep(req.user);
+        var myUser = _.cloneDeep(dummyUser);
+        req.user = _.assign(existingUser, myUser);
+        apos.customCodeEditor.getOptions(req, function (err, result) {
+            if (err) {
+                console.log("ERROR (GET) : ", err)
+            }
+            assert(!err)
+            expect(result).toMatchObject(body.customCodeEditor);
+            expect(Object.keys(result).length).toBe(1);
+            // Check User Database to be match with customCodeEditor saving options
+            apos.users.find(req, { username: "abuBakar" }).toObject(function (err, user) {
+                if (err) {
+                    console.log("ERROR (USER GET) : ", err)
+                }
+                assert(!err);
+                // Must match with the result
+                expect(user.customCodeEditor).toMatchObject(result);
+                assert(user.username === "abuBakar");
+                done();
+            })
+        })
+    });
+
+    it('should reset options successfully using DELETE route' , function(done) {
+        var req = apos.tasks.getReq();
+        var existingUser = _.cloneDeep(req.user);
+        var myUser = _.cloneDeep(dummyUser);
+        req.user = _.assign(existingUser, myUser);
+
+        apos.customCodeEditor.removeOptions(req, function(err) {
+            if(err){
+                console.log("ERROR (DELETE) : ", err);
+            }
+            assert(!err);
+            // Check user to see if it is truly removes all the options
+            apos.users.find(req, {username : "abuBakar"}).toObject(function(err, user) {
+                if(err){
+                    console.log("ERROR (USER DELETE) : ",err);
+                }
+                assert(!err);
+                assert(user.username === "abuBakar");
+                expect(user["customCodeEditor"]).toBe(undefined);
+                done();
+            })
+        });
+    });
+
+    it('should get empty saves options after removed all the options' , function(done) {
+        var req = apos.tasks.getReq();
+        var existingUser = _.cloneDeep(req.user);
+        var myUser = _.cloneDeep(dummyUser);
+        req.user = _.assign(existingUser, myUser);
+        apos.customCodeEditor.getOptions(req, function (err, result) {
+            if (err) {
+                console.log("ERROR (GET) : ", err)
+            }
+            assert(!err)
+            expect(result).toMatchObject({});
+            expect(Object.keys(result).length).toBe(0);
+            done();
+        })
+    })
 });
